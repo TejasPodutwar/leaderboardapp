3d1e47be09b92444022c89b36cd7557677557449
Tejas <tejaskumar8501@gmail.com>
2021-04-19 19:52:31 +0530

    App made ready to run locally

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 exports.homeRoute = (req,res)=>{
 
-    axios.get("https://cpleaderboard.herokuapp.com/api/users")
+    axios.get(`http://localhost:5005/api/users`)
     .then( result => {
         const sorted_data = result.data;
         res.render("index", {users: sorted_data}); 
@@ -18,7 +18,7 @@ exports.addUserRoute = (req,res) => {
 
 exports.updateUserRoute = (req,res) => {
     console.log(req.query.id);
-    axios.get("https://cpleaderboard.herokuapp.com/api/users", {params: {id: req.query.id}})
+    axios.get(`http://localhost:5005/api/users`, {params: {id: req.query.id}})
     .then( result => {
         res.render("update_user", {user : result.data});
     })
